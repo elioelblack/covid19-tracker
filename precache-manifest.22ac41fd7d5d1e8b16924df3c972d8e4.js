8c83a9b614881c410abcaf5d061ae39c118c99ee
eliezer droid <eliohernandez94@gmail.com>
2020-04-27 12:11:25 -0600

    Updates

--- precache-manifest.22ac41fd7d5d1e8b16924df3c972d8e4.js
+++ precache-manifest.22ac41fd7d5d1e8b16924df3c972d8e4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d15a0122b3ea21efa3b28d43541cea32",
+    "revision": "0820f522b4c85660c4a1f1ce564c6be4",
     "url": "/covid19-tracker/index.html"
   },
   {
-    "revision": "b4141bcb0d025fe2724b",
+    "revision": "faa21a576374ac5c4b70",
     "url": "/covid19-tracker/static/css/main.ad16c9b0.chunk.css"
   },
   {
-    "revision": "e3a47dd5170b993fa10e",
-    "url": "/covid19-tracker/static/js/2.a9ead44e.chunk.js"
+    "revision": "a63ca5ff7bdb4698d6ff",
+    "url": "/covid19-tracker/static/js/2.8c4b76ce.chunk.js"
   },
   {
     "revision": "176852ae27885dfd7559e0fb4b332d24",
-    "url": "/covid19-tracker/static/js/2.a9ead44e.chunk.js.LICENSE.txt"
+    "url": "/covid19-tracker/static/js/2.8c4b76ce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4141bcb0d025fe2724b",
-    "url": "/covid19-tracker/static/js/main.f4dcc2fc.chunk.js"
+    "revision": "faa21a576374ac5c4b70",
+    "url": "/covid19-tracker/static/js/main.d1c3c101.chunk.js"
   },
   {
     "revision": "3472a32df87a999f1966",
